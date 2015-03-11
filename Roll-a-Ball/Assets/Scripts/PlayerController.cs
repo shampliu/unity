e1cec19a2fb113f13ea09df2db004aa4cbe4d613
Chang Liu <shampliu@gmail.com>
2015-03-11 02:21:29 -0700

    first commit

---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed; // can edited in the Unity Editor
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// called before physics update
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical"); 
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical); 
+		GetComponent<Rigidbody>().AddForce (movement * speed * Time.deltaTime);
+		// multiplying by Time.deltaTime makes it smooth
+	} 
+}
